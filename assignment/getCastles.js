7cba02eebc1aea79c305ad3a6ad12ed209ba1a80
charonchan1991 <charonchan1991@gmail.com>
2017-11-06 23:16:48 -0800

    Assignment 1 done

---
+++ assignment/getCastles.js
@@ -0,0 +1,53 @@
+
+
+// returns an array of peak/valley values
+module.exports = function (seq) {
+
+  // if nothing in the array then simply returns 0
+  if (seq.length === 0) return 0;
+
+  var diff = 0;
+  var castles = [];
+
+  // always build a castle at the start
+  castles.push(seq[0])
+
+  // loop through each integer in the array
+  for (var i = 0; i < seq.length; i++) {
+    
+    // if not the last integer
+    if (i < seq.length - 1) {
+      // test the monotonicity at this point:
+      // if diffs on left and right have different signs, then it's a peak/valley
+      if ((seq[i+1] - seq[i]) * diff < 0) {
+        castles.push(seq[i]);
+      }
+      // if the next integer has the same value, continue search until find a different one or reach end of array
+      else if (seq[i+1] === seq[i] && diff !== 0) {
+        for (var j = i + 1; j < seq.length; j++) {
+          // test the monotonicity again to determine if it's a peak/valley
+          if (seq[i] !== seq[j]) {
+            if ((seq[j] - seq[i]) * diff < 0) {
+              castles.push(seq[i]);
+            }
+            break;
+          } else if (j === seq.length - 1) {
+            // end of array, no more different values, just return the final result
+            castles.push(seq[i]);
+            return castles;
+          }
+        }
+      }
+      // save the diff for next loop
+      diff = seq[i+1] - seq[i];
+    } else {
+      // end of array, always a peak/valley unless equals to the preceding number
+      if (diff !== 0) {
+        castles.push(seq[i]);
+      }
+    }
+
+  }
+  // return the total num of castles to be built
+  return castles;
+}
---
+++ test/getCastlesTest.js
@@ -0,0 +1,63 @@
+var assert = require("chai").assert;
+var getCastles = require('../assignment/getCastles');
+
+// feel free to add more examples to test the function
+var stretchOfLands = [
+  {
+    data: [1, 1],
+    expect: [1]
+  },
+  {
+    data: [1, 2],
+    expect: [1, 2]
+  },
+  {
+    data: [1, 2, 3, 3, 4],
+    expect: [1, 4]
+  },
+  {
+    data: [2, 6, 6, 6, 3],
+    expect: [2, 6, 3]
+  },
+  {
+    data: [2, 6, 6, 6, 6, 3, 2],
+    expect: [2, 6, 2]
+  },
+  {
+    data: [2, 6, 6, 6, 6, 3, 3, 3, 4],
+    expect: [2, 6, 3, 4]
+  },
+  {
+    data: [2, 6, 9, 6, 3],
+    expect: [2, 9, 3]
+  },
+  {
+    data: [2, 2, 2, -4, 5, 8, -3, 0, 3, 3, 3, 4, 4, 5, 1, 8],
+    expect: [2, -4, 8, -3, 5, 1, 8]
+  },
+  {
+    data: [5, 2, 5, 7, 2, 2, 6, 6, 6, 1, 5, 8, 8, 9],
+    expect: [5, 2, 7, 2, 6, 1, 9]
+  }
+];
+
+// test the function with the above data
+describe('ASSIGNMENT 1: THE CASTLE COMPANY:', function () {
+
+  stretchOfLands.forEach(function(stretchOfLand, index) {
+
+    describe('Castles for the land ' + JSON.stringify(stretchOfLand.data), function () {
+      
+      var result = getCastles(stretchOfLand.data);
+
+      it('should return ' + JSON.stringify(stretchOfLand.expect), function () {
+        assert.deepEqual(result, stretchOfLand.expect);
+      });
+
+      it('total number of castles to be built: ' + result.length);
+
+    });
+
+  });
+
+});
